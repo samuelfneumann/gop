3099936d9e3d281229476ea2344516aff3f937d6
Samuel Neumann <samuelfneumann@gmail.com>
2021-10-05 10:22:18 -0600

    Add unit test for passing gradient through clamp

--- op_clamp_test.go
+++ op_clamp_test.go
@@ -9,6 +9,109 @@ import (
 	"gorgonia.org/tensor"
 )
 
+// TestF64ClampPassGradient tests if the clamp operation works in the
+// forward pass on a float64 tensor and if the gradient is passed
+// through the operation when requested to do so.
+func TestF64ClampPassGradient(t *testing.T) {
+	const numTests int = 15     // The number of random tests to run
+	const clipScale float64 = 2 // Legal ranges generated based on clipScale
+	const scale float64 = 5     // Values are clamped based on scale
+
+	// Randomly generated input has number of dimensions between dimMin
+	// and dimMax. Each dimension of the randomly generated input has
+	// between sizeMin and sizeMax elements.
+	const sizeMin int = 1
+	const sizeMax int = 10
+	const dimMin int = 1
+	const dimMax int = 4
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < numTests; i++ {
+		var min float64 = clipScale * (rand.Float64() - 1) // [-clipScale, 0)
+		var max float64 = clipScale * rand.Float64()       // [0, clipScale)
+
+		// Construct the size of each dimension randomly, e.g. (3, 1, 2)
+		size := randInt(dimMin+rand.Intn(dimMax-dimMin), sizeMin, sizeMax)
+
+		// Get the total number of elements for the random input
+		numElems := tensor.ProdInts(size)
+
+		// Construct input data
+		inBacking := randF64(numElems, min*scale, max*scale)
+		inTensor := tensor.NewDense(
+			tensor.Float64,
+			size,
+			tensor.WithBacking(inBacking),
+		)
+
+		// Construct the target/correct gradient
+		gradTarget := make([]float64, numElems)
+		for i := range inBacking {
+			gradTarget[i] = 1
+
+		}
+
+		// Construct input node to be clamped
+		g := G.NewGraph()
+		in := G.NewTensor(
+			g,
+			tensor.Float64,
+			len(inTensor.Shape()),
+			G.WithValue(inTensor),
+		)
+
+		// Construct the clamp operation and save the outputted value
+		c, err := Clamp(in, min, max, true)
+		if err != nil {
+			t.Error(err)
+		}
+		var cVal G.Value
+		G.Read(c, &cVal)
+
+		// Construct loss + gradient
+		loss := G.Must(G.Sum(c))
+		grad, err := G.Grad(loss, in)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(grad) != 1 {
+			t.Errorf("expected 1 gradient got %v", len(grad))
+		}
+		var gradVal G.Value
+		G.Read(grad[0], &gradVal)
+
+		// Run the graph
+		vm := G.NewTapeMachine(g)
+		err = vm.RunAll()
+		if err != nil {
+			t.Error(err)
+		}
+		vm.Reset()
+
+		// Ensure the output is clamped properly
+		data := cVal.Data().([]float64)
+		for i, elem := range data {
+			if elem > max {
+				t.Errorf("value at index %d greater than maximum %v", i, max)
+			}
+			if elem < min {
+				t.Errorf("value at index %d less than minimum %v", i, min)
+			}
+		}
+
+		// Ensure the gradient was calculated correctly
+		gradData := gradVal.Data().([]float64)
+		for i := range gradTarget {
+			if gradTarget[i] != gradData[i] {
+				t.Errorf("expected: %v \nreceived: %v \nindex: %d",
+					gradTarget[i], gradData[i], i)
+			}
+		}
+
+		vm.Close()
+	}
+}
+
 // TestF64Clamp tests the clamp operation on a float64 tensor
 func TestF64Clamp(t *testing.T) {
 	const numTests int = 15     // The number of random tests to run
